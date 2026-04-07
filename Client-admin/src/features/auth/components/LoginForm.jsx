3eabb624bf4b3717034941b2ac549ffc59246eb0
Narizaico-code <areyes-2024435@kinal.edu.gt>
2026-04-07 17:18:06 -0600

    Hola 2

--- Client-admin/src/features/auth/components/LoginForm.jsx
+++ Client-admin/src/features/auth/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-export const LoginForm = () => {
+export const LoginForm = ({onForgot}) => {
   return (
     <div>
         <form action="" className="space-y-5">
@@ -15,6 +15,36 @@ export const LoginForm = () => {
                  />
                  
             </div>
+
+            <div>
+                <label htmlFor="password"
+                  className="block text-sm font-medium text-gray-700">
+                  Contraseña
+                </label>
+
+                <input type="password" 
+                    id="password"
+                    placeholder="contraseña123"
+                    className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                 />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-main-blue hover:opacity-90 text-white font-medium py-2.5 px-4 rounded-lg
+                          transition-colors duration-200 text-sm disabled:opacity-50"
+            >
+                Iniciar sesión
+            </button>
+
+            <p className="text-center text-sm">
+              <button 
+              type="button"
+              className="text-main-blue hover:underline"
+              onClick={onForgot}>
+                ¿Olvidaste tu contraseña?
+              </button>
+            </p>
         </form>
     </div>
   )
