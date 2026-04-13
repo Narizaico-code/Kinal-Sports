474de341015378a801faf0065585ba9a47b43db3
Narizaico-code <areyes-2024435@kinal.edu.gt>
2026-04-13 17:21:54 -0600

    hopla1212

--- Client-admin/src/features/auth/components/LoginForm.jsx
+++ Client-admin/src/features/auth/components/LoginForm.jsx
@@ -1,51 +1,104 @@
-export const LoginForm = ({onForgot}) => {
+import { useForm } from "react-hook-form";
+import { useAuthStore } from "../store/authStore";
+import { useNavigate } from "react-router-dom";
+
+export const LoginForm = ({ onForgot }) => {
+  const navigate = useNavigate();
+
+  const login = useAuthStore((state) => state.login);
+  const loading = useAuthStore((state) => state.loading);
+  const error = useAuthStore((state) => state.error);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    //Mandar informacion al backend a la hora de iniciar sesión
+    console.log(data);
+    const res = await login(data);
+
+    if (res?.success) {
+      navigate("/dashboard");
+    }
+  };
+
   return (
-    <div>
-        <form action="" className="space-y-5">
-            <div>
-                <label htmlFor="emailOrUsername"
-                className="block text-sm font-medium text-gray-700">
-                    Email o Usuario
-                </label>
-
-                <input type="text" 
-                    id="emailOrUsername"
-                    placeholder="correo@ejemplo.com o usuario"
-                    className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-                 />
-                 
-            </div>
-
-            <div>
-                <label htmlFor="password"
-                  className="block text-sm font-medium text-gray-700">
-                  Contraseña
-                </label>
-
-                <input type="password" 
-                    id="password"
-                    placeholder="contraseña123"
-                    className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-                 />
-            </div>
-
-            <button
-              type="submit"
-              className="w-full bg-main-blue hover:opacity-90 text-white font-medium py-2.5 px-4 rounded-lg
-                          transition-colors duration-200 text-sm disabled:opacity-50"
-            >
-                Iniciar sesión
-            </button>
-
-            <p className="text-center text-sm">
-              <button 
-              type="button"
-              className="text-main-blue hover:underline"
-              onClick={onForgot}>
-                ¿Olvidaste tu contraseña?
-              </button>
-            </p>
-        </form>
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+      {" "}
+      <div>
+        {" "}
+        <label
+          htmlFor="emailOrUsername"
+          className="block text-sm font-medium text-gray-800 mb-1.5"
+        >
+          Email o Usuario{" "}
+        </label>
+        {" "}
+        <input
+          id="emailOrUsername"
+          type="email"
+          placeholder="correo@ejemplo.com o usuario"
+          className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+          {...register("emailOrUsername", {
+            required: "Este campo es obligatorio",
+          })}
+        />
+        
+        {errors.emailOrUsername && (
+          <p className="text-red-600 text-sm">
+            {errors.emailOrUsername.message}
+          </p>
+        )}{" "}
+      </div>{" "}
+      <div>
+        {" "}
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-800 mb-1.5"
+        >
+          Contraseña{" "}
+        </label>{" "}
+        <input
+          id="password"
+          type="password"
+          placeholder="••••••••"
+          className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+          {...register("password", {
+            required: "Este campo es obligatorio",
+          })}
+        />
+        {errors.password && (
+          <p className="text-red-600 text-sm">{errors.password.message}</p>
+        )}
+        {" "}
+      </div>
+      
+      {error && <p className="text-red-600 text-xs mt-1">{error}</p>}           {" "}
+      <button
+        type="submit"
+        className="w-full bg-main-blue hover:opacity-90 text-white font-medium py-2.5 px-4 rounded-lg 
+            transition-colors duration-200 text-sm disabled:opacity-50"
+        disabled={loading}
+      >
+        {loading ? "Iniciando..." : "Iniciar Sesión"}
+        {" "}
+      </button>
+      {" "}
+      <p className="text-center text-sm">
+        {" "}
+        <button
+          type="button"
+          onClick={onForgot}
+          className="text-main-blue hover:underline"
+        >
+           ¿Olvidaste tu contraseña? {" "}
+        </button>
+        {" "}
+      </p>
+        {" "}
+    </form>
+  );
+};
--- Client-admin/src/features/auth/store/authStore.js
+++ Client-admin/src/features/auth/store/authStore.js
@@ -1,3 +1,40 @@
 import { create } from "zustand";
-import { persistence } from "zustand/middleware"
-import { login } from "../../../shared/api"
+import { persist } from "zustand/middleware"
+import { login as loginRequest } from "../../../shared/api"
+
+export const useAuthStore = create(persist(
+    (set, get) => ({
+        user: null,
+        token: null,
+        expiresAt: null,
+        loading: false,
+        error: null,
+        isAuthenticated: false,
+
+        login: async ({ emailOrUsername, password }) => {
+            try {
+                set({ loading: true, error: null });
+
+                const { data } = await loginRequest({ emailOrUsername, password });
+
+                set({
+                    user: data.userDetails,
+                    token: data.token,
+                    expiresAt: data.expiresAt,
+                    loading: false
+                })
+
+                return { success: true };
+            } catch (error) {
+                console.log("Login error:", error);
+                const message = 
+                error.response?.data?.message || "Error de autenticación.";
+                
+                set({ error: message, loading: false });
+                return { success: false, message };
+            }
+        }
+    }),
+    { name: "auth-storage" }
+)
+)
